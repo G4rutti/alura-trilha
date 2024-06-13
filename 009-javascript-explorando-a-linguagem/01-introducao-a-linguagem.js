af62bbc711811ec1bd353514a919bb56c228f9f7
G4rutti <davigarutti5@gmail.com>
2024-06-13 16:29:50 -0300

    Inicio do desafio e ao módulo 09 da trilha

---
+++ 009-javascript-explorando-a-linguagem/01-introducao-a-linguagem.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+console.log("Trabalhando com variáveis");
+
+const idade = 26;
+console.log(idade)
+console.log(idade + 2)
---
+++ 009-javascript-explorando-a-linguagem/02-tipos-de-variaveis.js
@@ -0,0 +1,16 @@
+console.log("Trabalhando com variáveis");
+
+
+// Const - um tipo de variável que nunca irá mudar:
+const idade = 18;
+const nome = "Davi";
+
+// Operações aritméticas:
+console.log(10 + 8 * 2); // 26
+console.log((10 + 8) * 2); // 36
+// * = mutiplicação | / = divisão | + = adição | - = subtração
+
+// Conversão de tipos
+let numTexto = "2";
+console.log(parseInt(numTexto)); // Transformar string em int
+console.log(parseFloat(parseInt(numTexto))); // Transformar o int em 2.0
