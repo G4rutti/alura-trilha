ae7ba9f59eadf0a5019b96d28dc436516111f04a
G4rutti <davigarutti5@gmail.com>
2024-06-17 09:13:53 -0300

    Módulo 9 finalizado

--- 009-javascript-explorando-a-linguagem/05-iterando.js
+++ 009-javascript-explorando-a-linguagem/05-iterando.js
@@ -2,18 +2,40 @@ const listaDeDestinos = new Array (
     "Salvador",
     "Rio de Janeiro",
     "São Paulo"
-)
+);
 
 
 const idadeComprador = 15;
-const estaAcompanhado = true
-const destino = "Curitiba"
-let temPassagemComprada = false
+const estaAcompanhado = true;
+const destino = "Curitiba";
+let temPassagemComprada = false;
+
+let contador = 0;
+let destinoExiste = false;
+
+while ( contador < 3) {
+    if(listaDeDestinos[contador] == destino){ 
+        console.log("Destino disponivel");
+        destinoExiste = true;
+        break;
+    }else{
+        console.log("Destino não disponivel");
+        destinoExiste = false;
+    }
+    contador += 1;
+}
+
+for(let i ; i < listaDeDestinos.length ; i++){
+    if(listaDeDestinos[contador] == destino){ 
+        console.log("Destino disponivel");
+        destinoExiste = true;
+        break;
+    }
+}
 
 if(idadeComprador >= 18 || estaAcompanhado){ 
     console.log("Pode passar!");
-    temPassagemComprada = true
+    temPassagemComprada = true;
 }else{
-
     console.log("Mete o pé!");
 }
